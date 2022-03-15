da4a56240abf18204713153407713ef35e3508fd
kuznetcov-ia <kuznetsov-yaa@ural.rt.ru>
2022-03-15 16:25:51 +0500

    Initial commit

---
+++ src/main/java/com/psu/econom/aspirant/default_project/DefaultProjectApplication.java
@@ -0,0 +1,13 @@
+package com.psu.econom.aspirant.default_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psu/econom/aspirant/default_project/DefaultProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psu.econom.aspirant.default_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
